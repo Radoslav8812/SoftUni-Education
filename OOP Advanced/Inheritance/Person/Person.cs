48c15fd14f81ea06a97d4d6d77f8e33a1492bdcb
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-03 14:54:52 +0200

    Person

---
+++ OOP Advanced/Inheritance/Person/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (this.age < 0)
+                {
+                    Console.WriteLine("Invalid Age!");
+                }
+                else
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {this.Name}, Age: {this.Age}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
