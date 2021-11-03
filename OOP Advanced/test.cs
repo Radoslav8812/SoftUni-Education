b489b11c21c1f6bf1226320d83889cf7d328ebd4
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-03 13:57:27 +0200

    Create test.cs

---
+++ OOP Advanced/test.cs
@@ -0,0 +1,14 @@
+namespace Restaurant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
