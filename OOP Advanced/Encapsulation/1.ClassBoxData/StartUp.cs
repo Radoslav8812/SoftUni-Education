22075eca36f0490a05400759f85cfcbbc5474d69
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-09 13:58:41 +0200

    Encapsulation

---
+++ OOP Advanced/Encapsulation/1.ClassBoxData/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1.ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+
+            if (box.Length > 0 && box.Width > 0 && box.Height > 0)
+            {
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }     
+        }
+    }
+}
