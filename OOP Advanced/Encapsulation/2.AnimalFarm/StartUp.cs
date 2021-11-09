1b6dfc2bba41adcefe86f5c771afb683b03636cf
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-09 14:13:38 +0200

    Encapsulation

---
+++ OOP Advanced/Encapsulation/2.AnimalFarm/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AnimalFarm;
+
+namespace _2.AnimalFarm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
