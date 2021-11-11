3478a097e66e4494f6e0aad55a06c7c871b62825
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-11 17:27:30 +0200

    Encapsulation

---
+++ OOP Advanced/Encapsulation/ShoppingSpree/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping_Spree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value != string.Empty && value != null)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.cost = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+            }
+        }
+    }
+}   
