f644beca8119492969da74a2dc0af0f241572a71
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-16 17:27:52 +0200

    Interfaces and Abstraction

---
+++ OOP Advanced/Interfaces and Abstraction/Food Shortage/Implementation/Citizen.cs
@@ -0,0 +1,29 @@
+﻿using FoodShortage.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodShortage.Implementation
+{
+    public class Citizen : IBuyer
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            BirthDate = birthdate;
+            Food = 0;
+        }
+        public string Name { get; private set; }
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string BirthDate { get; set; }
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            Food += 10;
+        }
+    }
+}
