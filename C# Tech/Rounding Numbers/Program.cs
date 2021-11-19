b6b995027fe6a0662fdd18207efed360d6cc107d
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-19 17:56:11 +0200

    C# Tech Module

---
+++ C# Tech/Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            double[] array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            foreach (double n in array)
+            {
+                int rounded = (int)Math.Round(n, MidpointRounding.AwayFromZero);
+                Console.WriteLine("{0} => {1}", n, rounded);
+            }
+        }
+    }
+}
