437c2e8af28477a3b2eea0826981b6832767a97f
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-20 20:51:28 +0200

    C# OOP

---
+++ C# OOP/Unit Tests/Summator/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    public class Summator
+    {
+        public int SumOfDigits(long n)
+        {
+            int sum = 0;
+            n = Math.Abs(n);
+            while (n != 0)
+            {
+                sum += (int)(n % 10);
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
