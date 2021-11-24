e694a8b7e86f21afe338de83ce175eb27b978c3e
Radoslav Bogdanov <radimk8812@abv.bg>
2021-11-24 22:58:24 +0200

    C# Education

---
+++ C# OOP/EXAMS/Aqua Shop/Models/Decorations/Decoration.cs
@@ -0,0 +1,39 @@
+﻿namespace AquaShop.Models.Decorations
+{
+    using AquaShop.Models.Decorations.Contracts;
+
+    public abstract class Decoration : IDecoration
+    {
+        private int comfort;
+        private decimal price;
+        protected Decoration(int comfort, decimal price)
+        {
+            this.Comfort = comfort;
+            this.Price = price;
+        }
+
+        public int Comfort
+        {
+            get
+            {
+                return this.comfort;
+            }
+            private set
+            {
+                this.comfort = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                this.price = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/EXAMS/Aqua Shop/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace AquaShop
+{
+    using AquaShop.Core;
+    using AquaShop.Core.Contracts;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+
+            System.Console.WriteLine(typeof(Engine).Name);
+            System.Console.WriteLine(engine.GetType().Name);
+        }
+    }
+}
