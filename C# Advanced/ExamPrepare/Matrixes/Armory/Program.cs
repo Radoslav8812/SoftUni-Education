5a6c42101710da181a1c91fb3b3ae179e341b04a
Radoslav Bogdanov <radimk8812@abv.bg>
2021-12-16 15:19:22 +0200

    C#

---
+++ C# Advanced/ExamPrepare/Matrixes/Armory/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace armory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+            int playerRow = -1;
+            int playerCol = -1;
+            int mirror1Row = -1;
+            int mirror1Col = -1;
+            int mirror2Row = -1;
+            int mirror2Col = -1;
+            //int playerRow1 = -1;
+            //int playerCol1 = -1;
+
+            double count = 0;
+
+            bool found = true;
+
+            var matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                    if (matrix[row, col] == 'A')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                        //playerRow1 = row;
+                        //playerCol1 = col;
+
+                    }
+                    if (matrix[row, col] == 'M')
+                    {
+                        if (found)
+                        {
+                            mirror1Row = row;
+                            mirror1Col = col;
+                            found = false;
+                           
+                        }
+                        else
+                        {
+                            mirror2Row = row;
+                            mirror2Col = col;
+                        }
+                    }
+                }
+            }
+
+
+            while (true)
+            {
+
+
+                var command = Console.ReadLine();
+                matrix[playerRow, playerCol] = '-';
+                if (command == "up")
+                {
+                    playerRow--;
+                }
+                else if (command == "down")
+                {
+                    playerRow++;
+                }
+                else if (command == "left")
+                {
+                    playerCol--;
+                }
+                else if (command == "right")
+                {
+                    playerCol++;
+                }
+
+
+                if (matrix.GetLength(0) <= playerRow || playerRow < 0)
+                {
+
+                    break;
+                }
+                if (matrix.GetLength(1) <= playerCol || playerCol < 0)
+                {
+                    break;
+
+                }
+
+                if (matrix[playerRow, playerCol] != '-' && matrix[playerRow, playerCol] != 'M')
+                {
+                    char num = matrix[playerRow, playerCol];
+                    var a  = Char.GetNumericValue(num);
+                    count += a;
+
+
+
+                    matrix[playerRow, playerCol] = 'A';
+                    if (count >= 65)
+                    {
+                        break;
+                    }
+                }
+                if (matrix[playerRow, playerCol] == 'M')
+                {
+                    if (playerRow == mirror1Row && playerCol == mirror1Col)
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow = mirror2Row;
+                        playerCol = mirror2Col;
+                        matrix[playerRow, playerCol] = 'A';
+                    }
+                    else if (playerRow == mirror2Row && playerCol == mirror2Col)
+                    {
+                        matrix[playerRow, playerCol] = '-';
+                        playerRow = mirror1Row;
+                        playerCol = mirror1Col;
+                        matrix[playerRow, playerCol] = 'A';
+                    }
+                }
+
+
+            }
+
+            if (count >= 65)
+            {
+                Console.WriteLine("Very nice swords, I will come back for more!");
+              
+            } else
+            {
+                Console.WriteLine("I do not need more swords!");
+            }
+            Console.WriteLine($"The king paid {count} gold coins.");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/ExamPrepare/Stacks and Queues/Black Smith/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BlackSmith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var que = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var stack = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            var Gladius = 70;
+            var Shamshir = 80;
+            var Katana = 90;
+            var Sabre = 110;
+            var Broadsword = 150;
+
+            var GladiusCount = 0;
+            var ShamshirCount = 0;
+            var KatanaCount = 0;
+            var SabreCount = 0;
+            var BroadswordCount = 0;
+
+
+            while (que.Count > 0 && stack.Count > 0)
+            {
+                var currSteel = que.Peek();
+                var currCarbon = stack.Peek();
+                var currSum = currSteel + currCarbon;
+
+                if (currSum == Gladius)
+                {
+                    GladiusCount++;
+                    que.Dequeue();
+                    stack.Pop();
+                }
+                else if (currSum == Shamshir)
+                {
+                    ShamshirCount++;
+                    que.Dequeue();
+                    stack.Pop();
+                }
+                else if (currSum == Katana)
+                {
+                    KatanaCount++;
+                    que.Dequeue();
+                    stack.Pop();
+                }
+                else if (currSum == Sabre)
+                {
+                    SabreCount++;
+                    que.Dequeue();
+                    stack.Pop();
+                }
+                else if (currSum == Broadsword)
+                {
+                    BroadswordCount++;
+                    que.Dequeue();
+                    stack.Pop();
+                }
+                else
+                {
+                    que.Dequeue();
+                    currCarbon += 5;
+                    stack.Pop();
+                    stack.Push(currCarbon);
+                }
+            }
+
+            var totalNumberOfSwords = GladiusCount + SabreCount + ShamshirCount + KatanaCount + BroadswordCount;
+
+            if (totalNumberOfSwords > 0)
+            {
+                Console.WriteLine($"You have forged {totalNumberOfSwords} swords.");
+            } else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+            if (que.Count > 0)
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ", que)}");
+            } else
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", stack)}");
+            }
+            else
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            if (BroadswordCount > 0)
+            {
+                Console.WriteLine($"Broadsword: {BroadswordCount}");
+            }
+            if (SabreCount > 0)
+            {
+                Console.WriteLine($"Sabre: {SabreCount}");
+            }
+            if (KatanaCount > 0)
+            {
+                Console.WriteLine($"Katana: {KatanaCount}");
+            }
+            if (ShamshirCount > 0)
+            {
+                Console.WriteLine($"Shamshir: {ShamshirCount}");
+            }
+            if (GladiusCount > 0)
+            {
+                Console.WriteLine($"Gladius: {GladiusCount}");
+            }
+        }
+    }
+}
