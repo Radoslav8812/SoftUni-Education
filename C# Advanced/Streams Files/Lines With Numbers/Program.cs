27c73f0ee1957e9720390485cdf92f616d143e28
Radoslav Bogdanov <radimk8812@abv.bg>
2021-12-23 01:08:14 +0200

    C#

---
+++ C# Advanced/Streams Files/Lines With Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace StreamAndFIles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader(@"Countries.txt");
+            using StreamWriter sw = new StreamWriter(@"CountriesWithLineNumbers.txt");
+
+            int rowNumber = 1;
+
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+                sw.WriteLine($"{rowNumber}. {line}");
+                rowNumber++;
+            }
+        }
+    }
+}
