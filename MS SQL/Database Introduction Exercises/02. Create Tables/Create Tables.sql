09fddd056bc3e00cbef92fe3261081d195192252
Radoslav Bogdanov <radimk8812@abv.bg>
2022-05-17 11:37:21 +0300

    C# DB

--- MS SQL/Database Introduction Exercises/02. Create Tables/Create Tables.sql
+++ MS SQL/Database Introduction Exercises/02. Create Tables/Create Tables.sql
@@ -3,10 +3,26 @@ USE Minions
 CREATE TABLE [Minions](
     [Id] INT PRIMARY KEY,
     [Name] NVARCHAR(50) NOT NULL,
-    [Age] INT NOT NULL
+    [Age] INT
 )
 
 CREATE TABLE [Towns](
     [Id] INT PRIMARY KEY,
     [Name] NVARCHAR(50)
 )
+
+ALTER TABLE [Minions]
+ADD [TownId] INT 
+
+ALTER TABLE [Minions]
+ADD FOREIGN KEY ([TownId]) REFERENCES [Towns]([Id])
+
+INSERT INTO [Towns] VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna')
+
+INSERT INTO [Minions] VALUES
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2)
