cda8a0f2d821df247f981ad7d72e864722ec83f7
Radoslav Bogdanov <radimk8812@abv.bg>
2022-05-20 12:13:04 +0300

    C# BD

---
+++ MS SQL/1. Database Introduction/21. Basic Select Some Fields/Basic Select Some FIelds.sql
@@ -0,0 +1,3 @@
+SELECT [Name] FROM [Towns] ORDER BY [Name] ASC
+SELECT [Name] From [Departments] ORDER BY [Name] ASC
+SELECT [FirstName], [LastName], [JobTitle], [Salary] FROM [Employees] ORDER BY [Salary] DESC
