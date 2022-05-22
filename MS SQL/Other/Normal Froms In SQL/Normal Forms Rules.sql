7a884f17d36905c2e8e0c0b55c0a78f96cf77e76
Radoslav Bogdanov <radimk8812@abv.bg>
2022-05-22 10:48:05 +0300

    C# DB

---
+++ MS SQL/Other/Normal Froms In SQL/Normal Forms Rules.sql
@@ -0,0 +1,8 @@
+Create DATABASE [NormalForms]
+
+-- 1) Every case must have only 1(single value) value inside.
+-- 2) Each column must have same data type (domain).
+-- 3) Each column must have own unique name.
+-- 4) The order of the data doesn't matter.
+-- 5) Correct dependency.
+-- 6) Every table must have 5 normal form rules and every table/relation. Must have PK and 1 more column (attribure) also.
