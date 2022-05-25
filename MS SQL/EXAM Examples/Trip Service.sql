505df293f0b7456c373b484b448615366af5c5a1
Radoslav Bogdanov <radimk8812@abv.bg>
2022-05-25 14:20:25 +0300

    C# DM EXAM

--- MS SQL/EXAM Examples/Trip Service.sql
+++ MS SQL/EXAM Examples/Trip Service.sql
@@ -1,3 +1,5 @@
+/* Crеate a database called TripService. You need to create 6 tables: */
+
 CREATE TABLE Cities(
     Id INT IDENTITY PRIMARY KEY NOT NULL,
     [Name] NVARCHAR(20),
@@ -48,3 +50,21 @@ CREATE TABLE AccountsTrips(
     Luggage INT NOT NULL,
     CHECK(Luggage >= 0)
 )
+
+
+/* 2. Insert
+Insert some sample data into the database.
+Write a query to add the following records into the corresponding tables. All Ids should be auto-generated.
+*/
+INSERT INTO Accounts VALUES
+('John', 'Smith', 'Smith', 34, '1975-07-21', 'j_smith@gmail.com'),
+('Gosho', NULL,	'Petrov', 11, '1978-05-16',	'g_petrov@gmail.com'),
+('Ivan', 'Petrovich', 'Pavlov',	59,	'1849-09-26', 'i_pavlov@softuni.bg'),
+('Friedrich', 'Wilhelm', 'Nietzsche', 2, '1844-10-15', 'f_nietzsche@softuni.bg')
+
+INSERT INTO Trips VALUES
+(101, '2015-04-12',	'2015-04-14', '2015-04-20', '2015-02-02'),
+(102, '2015-07-07',	'2015-07-15', '2015-07-22',	'2015-04-29'),
+(103, '2013-07-17',	'2013-07-23', '2013-07-24',	NULL),
+(104, '2012-03-17',	'2012-03-31', '2012-04-01',	'2012-01-10'),
+(109, '2017-08-07',	'2017-08-28', '2017-08-29',	NULL)
