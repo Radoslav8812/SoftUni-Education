52815da22eebbd3c61d28e56f674508b76c1412c
Radoslav Bogdanov <radimk8812@abv.bg>
2022-06-11 11:06:57 +0300

    C# DB

--- MS SQL/4. Build In Functions/Lecture/SQLQuery_3.sql
+++ MS SQL/4. Build In Functions/Lecture/SQLQuery_3.sql
@@ -5,9 +5,11 @@ SELECT [FirstName],
         LEFT([JobTitle], 6) AS [CutJobTitle] 
         FROM [Employees]
 
-
+CREATE VIEW [V_LectureFunctions] AS
 SELECT [FirstName],
         [LastName],
         [JobTitle],
         LEFT([JobTitle], 6) + REPLICATE('*', LEN([JobTitle]) - 6) AS [Covered]
         FROM [Employees]
+
+SELECT * FROM [V_LectureFunctions]
